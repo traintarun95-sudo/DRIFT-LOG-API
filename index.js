9daf93d74414b81db663e000109f444ddbd4db55
traintarun95-sudo <traintarun95@gmail.com>
2026-04-21 00:26:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,6 @@ const server = http.createServer(async (req, res) => {
 })
 
 const PORT = process.env.PORT || 3000
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`)
-})
+}) ho
